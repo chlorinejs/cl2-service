6469fe8b04795959949a3377dcea74b5d838b466
Hoang Minh Thang <p@banphim.net>
2013-03-10 00:41:06 +0700

    update to clojure 1.5.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject cl2-service "0.2.0-SNAPSHOT"
+(defproject cl2-service "0.3.0-SNAPSHOT"
   :description "Chlorine compiling service."
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [lib-noir "0.3.5"]
-                 [chlorine "1.7.0-SNAPSHOT"]
+                 [chlorine "1.7.0"]
                  [core-cl2 "1.0.0-SNAPSHOT"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler cl2-service.server/handler
--- src/cl2_service/views/compile.clj
+++ src/cl2_service/views/compile.clj
@@ -5,35 +5,6 @@
         [chlorine.js])
   (:import java.util.Date))
 
-(def angular {:reserved-symbols
-             [#"^\$.*"
-              #"^\.\$.*"]
-             :symbol-map
-             (array-map
-              #"^int$" "int*"
-              "$"  "$USD$"
-              "->" "$ARROW$"
-              "=>" "$BARROW$"
-              "<-" "$LARROW$"
-              ">"  "$GT$"
-              "<"  "$LT$"
-              ">=" "$GE$"
-              "=<" "$LE$"
-              "-"  "_"
-              "'"   "$QUOT$"
-              "!"  "$EXCL$"
-              "?"  "$QUEST$"
-              "#"  "$HASH$"
-              "%"  "$P100$"
-              "&"  "$AND$"
-              "*"  "$STAR$"
-              "+"  "$PLUS$"
-              "="  "$EQ$"
-              "|"  "$PIPE$"
-              )})
-
-(def profiles {"angular" angular})
-
 (defn now "Gets current time in miliseconds"
   [] (.getTime (Date.)))
 
@@ -44,8 +15,7 @@ and saves states to this var."}
             *last-sexpr*     (ref nil)
             *macros*         (ref {})
             *print-pretty*   true]
-    (let [core-js (with-profile angular
-                    (js (include! [:resource "dev.cl2"])))]
+    (let [core-js (js (include! [:resource "dev.cl2"]))]
       {:temp-sym-count @*temp-sym-count*
        :macros @*macros*
        :core-js core-js})))
@@ -62,13 +32,12 @@ and saves states to this var."}
   (GET "/service/:filename" [filename :as request]
        (when-let [referer (get-in  request [:headers "referer"])]
          (let [session (new-session)]
-           (with-profile angular
-             (binding [*temp-sym-count* (:temp-sym-count session)
-                       *last-sexpr*      (:last-sexpr session)
-                       *macros*          (:macros session)
-                       *print-pretty*   true]
-               (str "console.log('Compiled at: ', " (now) ");\n"
-                    (tojs (url-normalize (str referer filename)))))))))
+           (binding [*temp-sym-count* (:temp-sym-count session)
+                     *last-sexpr*      (:last-sexpr session)
+                     *macros*          (:macros session)
+                     *print-pretty*   true]
+             (str "console.log('Compiled at: ', " (now) ");\n"
+                  (tojs (url-normalize (str referer filename))))))))
   (GET "/core-cl2.js" []
        (:core-js preloaded))
   )

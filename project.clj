a03db43c7fd45e6e0602ccb976ef45a7b42f2a89
Hoang Minh Thang <p@banphim.net>
2013-06-12 01:59:44 +0700

    bump to v0.5.1

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject cl2-service "0.5.0"
+(defproject cl2-service "0.5.1"
   :description "Chlorine compiling service."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [lib-noir "0.3.5"]
-                 [chlorine "1.5.3.1"]
-                 [core-cl2 "0.7.2"]]
+                 [lib-noir "0.6.1"]
+                 [chlorine "1.5.4"]
+                 [core-cl2 "0.7.3"]]
   :resource-paths ["node_modules"]
-  :plugins [[lein-ring "0.8.2"]]
+  :plugins [[lein-ring "0.8.5"]]
   :ring {:handler cl2-service.server/handler
          :auto-reload? false}
   :aot :all

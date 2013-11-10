8afbcaf47b69280248da5c1b368a74acc7de2d12
Hoang Minh Thang <p@banphim.net>
2013-11-10 19:54:05 +0000

    update chlorine libraries

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cl2-service "0.6.0"
+(defproject cl2-service "0.6.0-SNAPSHOT"
   :description "Chlorine compiling service."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lib-noir "0.6.1"]
-                 [chlorine "1.6.2"]
-                 [core-cl2 "0.8.0"]]
+                 [chlorine "1.6.4-SNAPSHOT"]
+                 [core-cl2 "0.9.0-SNAPSHOT"]]
   :resource-paths ["node_modules"]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler cl2-service.server/handler

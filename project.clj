54554dd331769a3ec7407f97e9a41b853f0cbf08
Hoàng Minh Thắng <p@banphim.net>
2014-01-31 21:33:13 +0700

    use chlorine/prelude in project

--- project.clj
+++ project.clj
@@ -2,9 +2,7 @@
   :description "Chlorine compiling service."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lib-noir "0.6.1"]
-                 [chlorine "1.6.4-SNAPSHOT"]
-                 [core-cl2 "0.9.0-SNAPSHOT"]]
-  :resource-paths ["node_modules"]
+                 [chlorine/prelude "0.1.0-SNAPSHOT"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler cl2-service.server/handler
          :auto-reload? false}
